5511c94bd07b4250e352379834aa1c1a1d4a5ae9
ARTIST <154514127+wwwlbs22@users.noreply.github.com>
2024-04-13 00:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,11 +19,11 @@ OWNER = int(getenv("OWNER", 5916859256))
 OWNER_USERNAME = getenv("OWNER_USERNAME","sultan11100")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/AbhiModszYT/AnieXEricaMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/wwwlbs22/Songxpro")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN",None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Logs_Gban")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AM_YTSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LINKS_CHANNELL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Quiz2xf")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", 7000))
@@ -55,18 +55,18 @@ AMBOT = [
     "ᴘʀᴏᴄᴇꜱꜱɪɴɢ..",
 ]
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/14f189b47693354041cee.png")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/14f189b47693354041cee.png")
+PLAYLIST_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://telegra.ph/file/14f189b47693354041cee.png")
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+STREAM_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/14f189b47693354041cee.png"
 
 
 def time_to_seconds(time):
